192e0f420bbb67ea639a31804ddff15c1f74702d
baovic <thienbaoit@gmail.com>
2018-08-29 15:01:49 +0700

    vieclam

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@
 $(window).scroll(function(){
   if ($(this).scrollTop() > 0) {
       $('.tizi-header').addClass('tizi-fixed');
+      $('.toppage').fadeIn( "slow" );
   } else {
       $('.tizi-header').removeClass('tizi-fixed');
+      $('.toppage').fadeOut( "slow" );
   }
+});
+// scroll top
+var topBtn = $('.toppage');
+topBtn.click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
 });
